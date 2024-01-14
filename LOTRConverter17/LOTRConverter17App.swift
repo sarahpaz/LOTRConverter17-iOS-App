4f84bc1896cd3e44c1bef5f3b77d108cc61bc701
sarahpaz <sarah.paz@gmail.com>
2024-01-14 07:55:11 -0500

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Sarah Paz on 2024-01-14.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
