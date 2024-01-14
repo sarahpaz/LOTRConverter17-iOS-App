92e742e6cbbef633ba0dbdce5a9779def4d6f0a7
Sarah Paz <sarah.paz@gmail.com>
2024-01-14 09:09:37 -0500

    Add pseudo code for layout

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Background image
+            
+            VStack {
+                // Pony image icon
+                
+                // Currency exchange title text
+                
+                // Conversion section
+                HStack {
+                    // Left conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            //Currency image
+                            
+                            // Currency text
+                        }
+                        // Text field
+                    }
+                    
+                    // Equal sign
+                    
+                    // Right conversion section
+                    VStack {
+                        // Currency
+                        HStack {
+                            
+                            // Currency text
+                            //Currency image
+                            
+                        }
+                        // Text field
+                    }
+                    
+                }
+                // info button
+            }
         }
-        .padding()
     }
 }
 
