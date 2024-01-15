2904404e7a637559d243a9f1562ce4cf34484917
Sarah Paz <sarah.paz@gmail.com>
2024-01-14 22:30:06 -0500

    Format with PrettySwift

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -8,75 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        ZStack {
-            // Background image
-            Image(.background)
+  var body: some View {
+    ZStack {
+      // Background image
+      Image(.background)
+        .resizable()
+        .ignoresSafeArea()
+      VStack {
+        // Pony image icon
+        Image(.prancingpony)
+          .resizable()
+          .scaledToFit()
+          .frame(height: 200)
+        // Currency exchange title text
+        Text("Currency Exchange")
+          .font(.largeTitle)
+          .foregroundStyle(.white)
+        // Conversion section
+        HStack {
+          // Left conversion section
+          VStack {
+            // Currency
+            HStack {
+              //Currency image
+              Image(.silverpiece)
                 .resizable()
-                .ignoresSafeArea()
-            VStack {
-                // Pony image icon
-                Image(.prancingpony)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 200)
-                // Currency exchange title text
-                Text("Currency Exchange")
-                    .font(.largeTitle)
-                    .foregroundStyle(.white)
-                // Conversion section
-                HStack {
-                    // Left conversion section
-                    VStack {
-                        // Currency
-                        HStack {
-                            //Currency image
-                            Image(.silverpiece)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(height: 33)
-                            // Currency text
-                            Text("Silver Piece")
-                                .font(.headline)
-                                .foregroundStyle(.white)
-                        }
-                        // Text field
-                        Text("Textfield")
-                    }
-                    // Equal sign
-                    Image(systemName: "equal")
-                        .font(.largeTitle)
-                        .foregroundStyle(.white)
-                        .symbolEffect(.pulse)
-                    // Right conversion section
-                    VStack {
-                        // Currency
-                        HStack {
-                            // Currency text
-                            Text("Gold Piece")
-                                .font(.headline)
-                                .foregroundStyle(.white)
-                            //Currency image
-                            Image(.goldpiece)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(height: 33)
-                        }
-                        // Text field
-                        Text("Textfield")
-                    }
-                }
-                Spacer()
-                // info button
-                Image(systemName: "info.circle.fill")
-                    .font(.largeTitle)
-                    .foregroundStyle(.white)
+                .scaledToFit()
+                .frame(height: 33)
+              // Currency text
+              Text("Silver Piece")
+                .font(.headline)
+                .foregroundStyle(.white)
             }
-//            .border(.blue)
+            // Text field
+            Text("Textfield")
+          }
+          // Equal sign
+          Image(systemName: "equal")
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .symbolEffect(.pulse)
+          // Right conversion section
+          VStack {
+            // Currency
+            HStack {
+              // Currency text
+              Text("Gold Piece")
+                .font(.headline)
+                .foregroundStyle(.white)
+              //Currency image
+              Image(.goldpiece)
+                .resizable()
+                .scaledToFit()
+                .frame(height: 33)
+            }
+            // Text field
+            Text("Textfield")
+          }
         }
+        Spacer()
+        // info button
+        Image(systemName: "info.circle.fill")
+          .font(.largeTitle)
+          .foregroundStyle(.white)
+      }
+      //            .border(.blue)
     }
+  }
 }
 
-#Preview {
-    ContentView()
+#Preview{
+  ContentView()
 }
