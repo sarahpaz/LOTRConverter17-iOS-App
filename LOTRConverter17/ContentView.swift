1a967535e6e3c4f430d632b5512e3e7df5561a8f
Sarah Paz <sarah.paz@gmail.com>
2024-01-15 08:49:07 -0500

    Fix indents

--- LOTRConverter17/ContentView.swift
+++ LOTRConverter17/ContentView.swift
@@ -8,58 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State var showExchangeInfo = false
-
-  var body: some View {
-    ZStack {
-      // Background image
-      Image(.background)
-        .resizable()
-        .ignoresSafeArea()
-      VStack {
-        // Pony image icon
-        Image(.prancingpony)
-          .resizable()
-          .scaledToFit()
-          .frame(height: 200)
-        // Currency exchange title text
-        Text("Currency Exchange")
-          .font(.largeTitle)
-          .foregroundStyle(.white)
-        // Conversion section
-        HStack {
-          // Left conversion section
-          VStack {
-            // Currency
-            HStack {
-              //Currency image
-              Image(.silverpiece)
-                .resizable()
-                .scaledToFit()
-                .frame(height: 33)
-              // Currency text
-              Text("Silver Piece")
-                .font(.headline)
-                .foregroundStyle(.white)
-            }
-            // Text field
-            Text("Textfield")
-          }
-          // Equal sign
-          Image(systemName: "equal")
-            .font(.largeTitle)
-            .foregroundStyle(.white)
-            .symbolEffect(.pulse)
-          // Right conversion section
-          VStack {
-            // Currency
-            HStack {
-              // Currency text
-              Text("Gold Piece")
-                .font(.headline)
-                .foregroundStyle(.white)
-              //Currency image
-              Image(.goldpiece)
+    @State var showExchangeInfo = false
+    
+    var body: some View {
+        ZStack {
+            // Background image
+            Image(.background)
                 .resizable()
                 .ignoresSafeArea()
             VStack {
